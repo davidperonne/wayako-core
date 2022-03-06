0418b42e89261ca7665c2edfc0a89551c1eef13f
David PERONNE <david.peronne@wayako.com>
2022-03-06 17:49:08 +0100

    Css

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -17,10 +17,6 @@ if ( ! function_exists( 'wayako_core_scripts' ) ) :
 		$theme_version = wp_get_theme()->get( 'Version' );
 
 		wp_enqueue_style( 'wayako-core-styles', WAYAKO_CORE_PLUGIN_URI . '/assets/css/wayako-core.min.css', array(), $theme_version );
-	//	wp_enqueue_style( 'splide-styles', WAYAKO_CORE_PLUGIN_URI . '/vendor/splide/css/splide.min.css', array(), $theme_version );
-
-	//	wp_enqueue_script( 'isotope-scripts', WAYAKO_CORE_PLUGIN_URI . '/vendor/isotope/isotope.pkgd.min.js', array(), $theme_version, true );
-	//	wp_enqueue_script( 'splide-scripts', WAYAKO_CORE_PLUGIN_URI . '/vendor/splide/js/splide.min.js', array(), $theme_version, true );
 
 		if ( is_singular( 'post' ) || is_page() ) {
 
